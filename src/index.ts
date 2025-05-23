170f3dd92f16991e28db9da35eae4c93feb802ad
Freilyn Bernabe <freilyn@bernabe.dev>
2025-05-23 10:40:09 -0400

    feat: Allow multiple origins and remove logestic (TESTING)

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,6 @@ import cors from "@elysiajs/cors";
 import swagger from "@elysiajs/swagger";
 import { Elysia, t } from "elysia";
 import { rateLimit } from "elysia-rate-limit";
-import { Logestic } from "logestic";
 import { ALLOWED_ORIGINS, PORT } from "./helpers/constants";
 import { generatePalette } from "./services/gemini";
 
@@ -23,7 +22,7 @@ app.use(
     },
   })
 );
-app.use(Logestic.preset("fancy"));
+// app.use(Logestic.preset("fancy"));
 
 app.post(
   "/generate-palette",
